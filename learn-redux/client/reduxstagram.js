cee4979dcd9ac0b1fbe248c8dae2afcd684524e5
John Kinane <jkinane@tenetpartners.com>
2019-08-11 18:57:35 -0400

    progress

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,6 +2,8 @@ import React from "react";
 import { render } from "react-dom";
 import css from "./styles/style.styl";
 import Main from './components/Main';
+import Main from './components/Single';
+import Main from './components/PhotoGrid';
 
 render(<Main />, document.getElementById('root'));
 
